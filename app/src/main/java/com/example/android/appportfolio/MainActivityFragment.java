e68aeb30725dfcefff65f72e508252b8d2e9c25a
Alexandre Dieudonne <adieudonne@gmail.com>
2015-12-07 22:55:23 -0500

    trying to get toasts to work, but broke the whole app

--- app/src/main/java/com/example/android/appportfolio/MainActivityFragment.java
+++ app/src/main/java/com/example/android/appportfolio/MainActivityFragment.java
@@ -1,35 +1,68 @@
 package com.example.android.appportfolio;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ListView;
 import android.widget.Toast;
-
-import java.util.ArrayList;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
 
+    Toast button_msg = new Toast(getContext());
     public MainActivityFragment() {
-    }
-    int duration = Toast.LENGTH_SHORT;
 
-    Toast button_msg = Toast(getContext());
+    }
+    //int duration = Toast.LENGTH_SHORT;
+
+
 
     //Runs when one of the ListView buttons is clicked
     public void printPurpose (View view){
-        button_msg.setView(view);
 
-        button_msg.setText(R.string.button_toast_msg + getText((Button) view.findViewById(R.id.list_item_portfolio_button) ) );
+        button_msg.setView(view);
+        Button buttonShell = (Button) view;
+        CharSequence buttonText = buttonShell.getText();
+        String castedText = buttonText.toString();
+        button_msg.setText(R.string.button_toast_msg + castedText);
         button_msg.show();
+
+        /*
+        switch  (view.getId())
+        {
+            case R.id.button0:
+                Button buttonShell = (Button) view;
+                CharSequence buttonText = buttonShell.getText();
+                String castedText = buttonText.toString();
+                button_msg.setText(R.string.button_toast_msg + castedText);
+                button_msg.show();
+                        break;
+            case R.id.button1:
+                displayToast(R.string.button_toast_msg + R.id.button0);
+                        break;
+            case R.id.button2:
+                displayToast(R.string.button_toast_msg + R.id.button);
+                        break;
+            case R.id.button3:
+                displayToast(R.string.button_toast_msg + R.id.button);
+                        break;
+            case R.id.button4:
+                displayToast(R.string.button_toast_msg + R.id.button);
+                        break;
+            case R.id.button5:
+                displayToast(R.string.button_toast_msg + R.id.button);
+                        break;
+
+        };
+
+        String buttonName = new String(this.getText());
+        button_msg.setText(R.string.button_toast_msg );
+        button_msg.show();
+        */
     }
 
     @Override
@@ -38,40 +71,7 @@ public class MainActivityFragment extends Fragment {
         //Make sure to create View to inflate for future searches
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
-        //Create ArrayList of Button objects to populate the listview with
-        ArrayList<Button> portAppList = new ArrayList<Button>();
-        portAppList.add(0, new Button(getContext()));
-        portAppList.get(0).setText(getString(R.string.button_text0));
-        portAppList.add(1, new Button(getContext()));
-        portAppList.get(1).setText(getString(R.string.button_text1));
-        portAppList.add(2, new Button(getContext()));
-        portAppList.get(2).setText(getString(R.string.button_text2));
-        portAppList.add(3, new Button(getContext()));
-        portAppList.get(3).setText(getString(R.string.button_text3));
-        portAppList.add(4, new Button(getContext()));
-        portAppList.get(4).setText(getString(R.string.button_text4));
-        portAppList.add(5, new Button(getContext()));
-        portAppList.get(5).setText(getString(R.string.button_text5));
 
-
-        //TIme to create the arrayAdapter
-        ArrayAdapter<Button> aPortfolioList = new ArrayAdapter<Button>(
-                //first argument is a context. Use function getcontext() to use the current context
-                getContext(),
-                //Second Argument is the layout you created for the Button.
-                R.layout.list_item_portfolio,
-                //Third argument is ith ID you gave the button in the layout above
-                R.id.list_item_portfolio_button,
-                //Finally include the name of the ArrayList created above.
-                portAppList
-        );
-
-
-        // Create a reference to the ListView so we cam bind the adapter to it.
-        ListView my_listview_porfolio = (ListView) rootView.findViewById(R.id.listview_portfolio_app);
-
-        //Now bind the Listview to the adapter
-        my_listview_porfolio.setAdapter(aPortfolioList);
         return rootView;
     }
 }
